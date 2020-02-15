f4c0721ac041658963d4375017e73cd91b2061bd
Logon Aluno <logonaluno@local.com>
2020-02-15 13:59:23 -0300

    Iniciando Projeto cervejaria batch tasklet

---
+++ cervejaria-batch-tasklet/src/main/java/br/com/fiap/cervejariabatchtasklet/CervejariaBatchTaskletApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cervejariabatchtasklet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CervejariaBatchTaskletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CervejariaBatchTaskletApplication.class, args);
+	}
+
+}
