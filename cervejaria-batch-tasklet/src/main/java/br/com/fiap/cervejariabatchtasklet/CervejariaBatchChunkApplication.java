6e0acaf81247feb0767969596f75f5f52de03358
Logon Aluno <logonaluno@local.com>
2020-02-15 16:55:55 -0300

    Spring Aula Sábado

---
+++ cervejaria-batch-tasklet/src/main/java/br/com/fiap/cervejariabatchtasklet/CervejariaBatchChunkApplication.java
@@ -0,0 +1,92 @@
+package br.com.fiap.cervejariabatchtasklet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.nio.file.Paths;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class CervejariaBatchChunkApplication {
+
+	Logger logger = LoggerFactory.getLogger(CervejariaBatchChunkApplication.class);
+
+	@Bean
+	public FlatFileItemReader<Pessoa> itemReader(@Value("${file.input}") Resource resource){
+		return new FlatFileItemReaderBuilder<Pessoa>()
+				.delimited().delimiter(";").names("name", "cpf")
+				.resource(resource)
+				.targetType(Pessoa.class)
+				.name("File Item Reader")
+				.build();
+	}
+
+	@Bean
+	public ItemProcessor<Pessoa, Pessoa> itemProcessor(){
+		return pessoa -> {
+			pessoa.setNome(pessoa.getNome().toUpperCase());
+			pessoa.setCpf(pessoa.getCpf()
+					.replaceAll("\\.", "")
+					.replace("-", ""));
+			return pessoa;
+		};
+	}
+
+	@Bean
+	public ItemWriter<Pessoa> itemWriter(DataSource dataSource){
+		return new JdbcBatchItemWriterBuilder<Pessoa>()
+				.beanMapped()
+				.dataSource(dataSource)
+				.sql("insert into tb_pessoa (nome, cpf) values (:nome, :cpf)")
+				.build();
+	}
+
+	@Bean
+	@Qualifier("stepChunk")
+	public Step step(StepBuilderFactory stepBuilderFactory,
+					 ItemReader<Pessoa> itemReader,
+					 ItemProcessor<Pessoa, Pessoa> itemProcessor,
+					 ItemWriter<Pessoa> itemWriter){
+
+		return stepBuilderFactory.get("step processar pessoa")
+				.<Pessoa, Pessoa>chunk(2)
+				.reader(itemReader)
+				.processor(itemProcessor)
+				.writer(itemWriter)
+				.build();
+	}
+
+	@Bean
+	public Job job(JobBuilderFactory jobBuilderFactory,
+				   @Qualifier("stepChunk") Step step){
+		return jobBuilderFactory.get("job processar pessoa")
+				.start(step)
+				.build();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CervejariaBatchChunkApplication.class, args);
+	}
+
+}
--- cervejaria-batch-tasklet/src/main/java/br/com/fiap/cervejariabatchtasklet/CervejariaBatchTaskletApplication.java
+++ cervejaria-batch-tasklet/src/main/java/br/com/fiap/cervejariabatchtasklet/CervejariaBatchTaskletApplication.java
@@ -1,10 +1,52 @@
 package br.com.fiap.cervejariabatchtasklet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.File;
+import java.nio.file.Paths;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class CervejariaBatchTaskletApplication {
+	Logger logger = LoggerFactory.getLogger(CervejariaBatchTaskletApplication.class);
+
+//	@Bean
+//	public Tasklet tasklet(@Value("${file.path}") String path){
+//		return (stepContribution, chunkContext) -> {
+//			File file = Paths.get(path).toFile();
+//			if(file.delete()){
+//				logger.info("Arquivo Deletadp.");
+//			}
+//			else{
+//				logger.error("Não foi possível  Deletar o arquivo");
+//			}
+//			return RepeatStatus.FINISHED;
+//		};
+//	}
+//
+//	@Bean
+//	public Step step(StepBuilderFactory stepBuilderFactory, Tasklet tasklet){
+//		return stepBuilderFactory.get("Delete File Step3").tasklet(tasklet)
+//				.allowStartIfComplete(true)
+//				.build();
+//	}
+//
+//	@Bean
+//	public Job job(JobBuilderFactory jobBuilderFactory, Step step){
+//		return jobBuilderFactory.get("Delete File Step3").start(step).build();
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CervejariaBatchTaskletApplication.class, args);
